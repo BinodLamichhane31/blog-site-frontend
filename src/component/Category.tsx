8002cdcfbc4104539541789ad63e3e550426dd2f
binodlamiochhane31 <lamibinod31@gmail.com>
2024-07-19 18:38:20 +0545

    Scrool bar for category

--- src/component/Category.tsx
+++ src/component/Category.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import {Button, ListGroup, ListGroupItem} from 'reactstrap'
-import {loadAllCategories} from "../services/category_service.tsx";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { Button } from 'reactstrap';
+import { loadAllCategories } from "../services/category_service.tsx";
+import './category.css';  // Import the CSS file for styling
+
 function Category() {
 
-    const [categories, setCategories] = useState([])
+    const [categories, setCategories] = useState([]);
 
     useEffect(() => {
         loadAllCategories().then(response => {
@@ -19,16 +19,14 @@ function Category() {
         });
     }, []);
 
-
-
     return (
-        <div className={'text-center'}>
-            <Button className={'me-2 category-button'} outline>All</Button>
+        <div className='category-container'>
+            <Button className='category-button mt-2' outline>All</Button>
             {categories && categories.map((category, index) => (
-                <Button key={index} className={'me-2 category-button'} outline>{category.categoryTitle}</Button>
+                <Button key={index} className='category-button mt-2' outline>{category.categoryTitle}</Button>
             ))}
         </div>
-    )
+    );
 }
 
-export default Category
+export default Category;
