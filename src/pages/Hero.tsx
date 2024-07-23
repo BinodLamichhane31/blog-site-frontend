40d3ad8b3329153e1ad8b40097e97f153c99c688
binodlamiochhane31 <lamibinod31@gmail.com>
2024-07-23 16:54:14 +0545

    Landing page

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,34 +1,49 @@
 import Base from "../component/Base.tsx";
-import {Button, Container} from "reactstrap";
-import {Link} from "react-router-dom";
-import LatestBlogs from "../component/LatestBlogs.tsx";
+import { Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import './Forms.css';
 
 function Hero() {
-
-
-    return(
+    return (
         <Base>
-            <div className="landing-page">
-                <Container className="text-center mt-3">
-                    <h1 className="landing-title">Welcome to Blog31</h1>
-                    <p className="landing-subtitle">Discover and share amazing stories with the world.</p>
-                    <div className="button-group">
-                        <Button  className="custom-button" outline
-                                 tag={Link}
-                                 to={'/home'}
-                        >Explore</Button>
-                        <Button className="landing-button"
-                                tag={Link}
-                                to={'/user/dashboard'}
-                        >Create</Button>
+            <div className={'hero-section'}>
+                <div className={'text-center'}>
+                    <h1>Welcome to Blog31</h1>
+                    <p>Discover and share amazing stories with the world.</p>
+                    <div className={'button-group'}>
+                        <Button className="custom-button-landing" outline tag={Link} to={'/home'}>
+                            Explore
+                        </Button>
+                        <Button className="landing-button" tag={Link} to={'/user/dashboard'}>
+                            Create
+                        </Button>
                     </div>
-                </Container>
+                </div>
+                <div className={'images-container'}>
+                    <div className={'image-item'}>
+                        <div className={'circle'}></div>
+                        <img src={'hero1.jpg'} alt={''} />
+                    </div>
+                    <div className={'image-item'}>
+                        <img src={'/hero2.webp'} alt={''} />
+                        <div className={'cylinder'} style={{ backgroundColor: '#81d8d0' }}></div> {/* Tiffany blue */}
+                    </div>
+                    <div className={'image-item'}>
+                        <div className={'cylinder'} style={{ backgroundColor: '#a0d6b4' }}></div> {/* Light greenish Tiffany shade */}
+                        <img src={'hero3.webp'} alt={''} />
+                        <img src={'hero4.jpeg'} alt={''} />
+                    </div>
+                    <div className={'image-item'}>
+                        <img src={'hero5.jpg'} alt={''} />
+                        <div className={'cylinder'} style={{ backgroundColor: '#b2e7e8' }}></div> {/* Light cyan Tiffany shade */}
+                    </div>
+                    <div className={'image-item'}>
+                        <img src={'hero1.jpg'} alt={''} />
+                    </div>
+                </div>
             </div>
-
         </Base>
-
-    )
-
+    );
 }
 
-export default Hero
+export default Hero;
